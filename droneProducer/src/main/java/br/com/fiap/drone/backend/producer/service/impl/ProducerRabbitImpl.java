d9aec86850f2a522ff5a1756ee0fbe0b8c3a58ae
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2020-09-27 23:06:41 -0300

    ultimos ajustes

--- droneProducer/src/main/java/br/com/fiap/drone/backend/producer/service/impl/ProducerRabbitImpl.java
+++ droneProducer/src/main/java/br/com/fiap/drone/backend/producer/service/impl/ProducerRabbitImpl.java
@@ -2,6 +2,10 @@ package br.com.fiap.drone.backend.producer.service.impl;
 
 import br.com.fiap.drone.backend.producer.models.DroneData;
 import br.com.fiap.drone.backend.producer.service.AmqpProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ProducerRabbitImpl implements AmqpProducer<DroneData> {
+
+    private Logger logger = LoggerFactory.getLogger(ProducerRabbitImpl.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -22,7 +28,13 @@ public class ProducerRabbitImpl implements AmqpProducer<DroneData> {
 
     @Override
     public void producer(DroneData droneData) {
-
+        String json = null;
+        try {
+            json = new ObjectMapper().writeValueAsString(droneData);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        logger.info("Produzindo: " + json);
         try {
 
             rabbitTemplate.convertAndSend(exchange, queue, droneData);
